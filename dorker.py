28ec296afa138d6478e9661edb6ab16174b11a88
0xdln1 <0xdlnp.gmail.com>
2021-07-11 23:28:44 +0530

    change typo

--- dorker.py
+++ dorker.py
@@ -17,7 +17,7 @@ def banner():
     | |/ /\ \_/ / |\ \| |\  \| |___| |\ \ 
     |___/  \___/\_| \_\_| \_/\____/\_| \_|             
 
-        Created by @0xdln and @0xrj3                                           
+        Created by @0xdln and @0xrj_
     """)
 
 def parser_error(self):
